759bbe573f801d8b9ea415a80a73a5bc4d0a3aa6
xverx <44094290+xverx@users.noreply.github.com>
2018-12-31 15:30:17 +0800

    Update Project-3.cpp

--- Project-3.cpp
+++ Project-3.cpp
@@ -2,28 +2,34 @@ class Student
 {
     public: 
         void makeMove(int Record[5][6], int Max[5][6], Color color[5][6], Color inputColor)
-		{
+	{
              //Your Code
-             for(int i=4; i>=0; i--)
-             for(int j=5; j>=0; j--)
-             {
-             	  if(Record[i][j] == 0)
-             	 {	
-             	       x = i; 
-	 					         y = j;
-	 					         break;
-					     }
-					      else if(color[i][j] == inputColor)
-					     {
-				 	          x = i;
-						        y = j;
-						        break;
-					      }
-		 		     }
-     }
+             int z = 0;
+             while(z<1)
+	     {
+             	for(int i=4; i>=0; i--)
+             	for(int j=5; j>=0; j--)
+             	{
+             	    if(Record[i][j] == 0)
+             	    {	
+             	        x = i; 
+	 		y = j;
+	 		z = 1;
+	 		break;
+		    }
+		    else if(color[i][j] == inputColor)
+		    {
+			x = i;
+			y = j;
+			z = 1;
+			break;
+		     }
+		 }
+	     }
+       	}
         //Any Code You Want to Add
         int getX()
-		{
+	{
              //Your Code
              return x;
         }
