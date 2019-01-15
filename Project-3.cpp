e30d7c3514f993fa3ecff467af86ee138549864e
xverx <44094290+xverx@users.noreply.github.com>
2019-01-15 20:55:27 +0800

    Update Project-3.cpp

--- Project-3.cpp
+++ Project-3.cpp
@@ -1,35 +1,69 @@
 class Student
 {
-    public: 
-        void makeMove(int Record[5][6], int Max[5][6], Color color[5][6], Color inputColor)
-	{
+      public:
+      void makeMove(int Record[5][6], int Max[5][6], Color color[5][6], Color inputColor)
+		{
              //Your Code
+             int i,j;
              int z = 0;
              while(z<1)
-	     {
-             	for(int i=4; i>=0; i--)
-             	for(int j=5; j>=0; j--)
+             {
+             	if(Record[0][0] == 0)
              	{
-             	    if(Record[i][j] == 0)
-             	    {	
-             	        x = i; 
-	 		y = j;
-	 		z = 1;
-	 		break;
-		    }
-		    else if(color[i][j] == inputColor)
-		    {
-			x = i;
-			y = j;
-			z = 1;
-			break;
-		     }
+             		x = 0;
+             		y = 0;
+             		z = 1;
+             		break;
+	 			}
+	 			
+	 			if(Record[0][5] == 0)
+             	{
+             		x = 0;
+             		y = 5;
+             		z = 1;
+             		break;
+	 			}
+	 			
+	 			if(Record[4][0] == 0)
+             	{
+             		x = 4;
+             		y = 0;
+             		z = 1;
+             		break;
+	 			}
+	 			
+	 			if(Record[4][5] == 0)
+             	{
+             		x = 4;
+             		y = 5;
+             		z = 1;
+             		break;
+	 			}
+             	for(i=0; i<=4; i++)
+             	{
+             		for(j=0; j<=5; j++)
+             		{
+             	        	if(Record[i][j] == 0)
+             			{		
+             	            			    x = i; 
+	 					    y = j;
+	 					    z = 1;
+	 					    break;
+				}
+				else if(color[i][j] == inputColor)
+				{
+				 	        x = i;
+						    y = j;
+						    z = 1;
+						    break;
+				}
+			 }
 		 }
-	     }
-       	}
+	   }
+      }
         //Any Code You Want to Add
         int getX()
-	{
+		{
              //Your Code
              return x;
         }
