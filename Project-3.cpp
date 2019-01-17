a4f4012b49029168ae88ecbde1f6a7ede89194d8
xverx <44094290+xverx@users.noreply.github.com>
2019-01-17 16:13:28 +0800

    Update Project-3.cpp

--- Project-3.cpp
+++ Project-3.cpp
@@ -1,7 +1,7 @@
-class Student
+class Student //暴力人定勝天法
 {
-      public:
-      void makeMove(int Record[5][6], int Max[5][6], Color color[5][6], Color inputColor)
+    public:
+	void makeMove(int Record[5][6], int Max[5][6], Color color[5][6], Color inputColor)
 		{
              //Your Code
              int i,j;
@@ -12,7 +12,6 @@ class Student
              	{
              		x = 0;
              		y = 0;
-             		z = 1;
              		break;
 	 			}
 	 			
@@ -20,7 +19,6 @@ class Student
              	{
              		x = 0;
              		y = 5;
-             		z = 1;
              		break;
 	 			}
 	 			
@@ -28,7 +26,6 @@ class Student
              	{
              		x = 4;
              		y = 0;
-             		z = 1;
              		break;
 	 			}
 	 			
@@ -36,31 +33,144 @@ class Student
              	{
              		x = 4;
              		y = 5;
-             		z = 1;
              		break;
 	 			}
-             	for(i=0; i<=4; i++)
+	 			
+	 			if( (Record[0][2] == 0) || ( (Record[0][2] == 1)  &&  (color[0][2] == inputColor) ) )
              	{
-             		for(j=0; j<=5; j++)
+             		x = 0;
+             		y = 2;
+             		break;
+	 			}
+	 			
+	 			if( (Record[4][2] == 0) || ( (Record[4][2] == 1)  &&  (color[4][2] == inputColor) ) )
+             	{
+             		x = 4;
+             		y = 2;
+             		break;
+	 			}
+	 			
+	 			if( (Record[2][0] == 0) || ( (Record[2][0] == 1)  &&  (color[2][0] == inputColor) ) )
+	 			{
+	 				x = 2;
+	 				y = 0;
+	 				break;
+ 				}
+	 			
+	 			if( (Record[2][5] == 0) || ( (Record[2][5] == 1)  &&  (color[2][5] == inputColor) ) )
+	 			{
+	 				x = 2;
+	 				y = 5;
+	 				break;
+ 				}
+ 				
+ 				if( (Record[1][1] == 0) || ( (Record[1][1] < 3)  &&  (color[1][1] == inputColor) ) )
+	 			{
+	 				x = 1;
+	 				y = 1;
+	 				break;
+ 				}
+ 				
+ 				if( (Record[3][4] == 0) || ( (Record[3][4] < 3)  &&  (color[3][4] == inputColor) ) )
+	 			{
+	 				x = 3;
+	 				y = 4;
+	 				break;
+ 				}
+ 				
+ 				if( (Record[1][3] == 0) || ( (Record[1][3] < 3)  &&  (color[1][3] == inputColor) ) )
+	 			{
+	 				x = 1;
+	 				y = 3;
+	 				break;
+ 				}
+ 				
+ 				if( (Record[3][2] == 0) || ( (Record[3][2] < 3)  &&  (color[3][2] == inputColor) ) )
+	 			{
+	 				x = 3;
+	 				y = 2;
+	 				break;
+ 				}
+ 				
+ 				if( (Record[1][4] == 0) || ( (Record[1][4] < 2)  &&  (color[1][4] == inputColor) ) )
+	 			{
+	 				x = 1;
+	 				y = 4;
+	 				break;
+ 				}
+ 				
+ 				if( (Record[3][1] == 0) || ( (Record[3][1] < 2)  &&  (color[3][1] == inputColor) ) )
+	 			{
+	 				x = 3;
+	 				y = 1;
+	 				break;
+ 				}
+ 				
+ 				if( (Record[4][2] < 3)  &&  (color[4][2] == inputColor) )
+	 			{
+	 				x = 4;
+	 				y = 2;
+	 				break;
+ 				}
+ 				
+ 				if( (Record[3][4] < 4)  &&  (color[3][4] == inputColor) )
+	 			{
+	 				x = 3;
+	 				y = 4;
+	 				break;
+ 				}
+ 				
+ 				if( (Record[2][2] < 4)  &&  (color[2][2] == inputColor) )
+	 			{
+	 				x = 2;
+	 				y = 2;
+	 				break;
+ 				}
+ 				
+ 				if( (Record[1][0] == 0) || ( (Record[1][0] < 2)  &&  (color[1][0] == inputColor) ) )
+             	{
+             		x = 1;
+             		y = 0;
+             		break;
+	 			}
+	 			
+	 			if( (Record[3][1] == 0) || ( (Record[3][1] < 4)  &&  (color[3][1] == inputColor) ) )
+             	{
+             		x = 3;
+             		y = 1;
+             		break;
+	 			}
+	 			if( (Record[1][5] == 0) || ( (Record[1][5] < 4)  &&  (color[1][5] == inputColor) ) )
+             	{
+             		x = 1;
+             		y = 5;
+             		break;
+	 			}
+ 				
+	 			
+	 				for(i=0; i<=4; i++)
              		{
-             	        	if(Record[i][j] == 0)
-             			{		
-             	            			    x = i; 
+             		for(j=0; j<=5; j++)
+             		    {
+             	            if(Record[i][j] == 0)
+             			    {		
+             	            x = i; 
 	 					    y = j;
 	 					    z = 1;
 	 					    break;
-				}
-				else if(color[i][j] == inputColor)
-				{
+					        }
+					        else if(color[i][j] == inputColor)
+					        {
 				 	        x = i;
 						    y = j;
 						    z = 1;
 						    break;
-				}
+					        }
+				        }
+		 			}
+             	
 			 }
-		 }
-	   }
-      }
+        }
         //Any Code You Want to Add
         int getX()
 		{
